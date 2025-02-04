3b9e7a3e868f8c713d612bdcb6480ff8f8ed3f3d
Komal Meena <capkomal2203@gmail.com>
2025-02-04 23:04:01 +0530

    Update Chat.jsx

--- vite-project/src/Chat.jsx
+++ vite-project/src/Chat.jsx
@@ -21,7 +21,9 @@ export default function Chat() {
   }, [])
 
   function connectToWs() {
-    const ws = new WebSocket(VITE_WEBSOCKET_URL)
+    // const ws = new WebSocket(VITE_WEBSOCKET_URL)
+    const ws = new WebSocket(import.meta.env.VITE_WEBSOCKET_URL);
+
     setWs(ws)
     ws.addEventListener('open', () => {
       console.log('WebSocket connected')
